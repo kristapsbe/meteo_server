36b6c7e0e81d070cf1495e4193fe52260e85d0f7
Kristaps <kbebris+git@gmail.com>
2024-10-11 21:24:31 +0300

    updated locust with params

--- locustfile.py
+++ locustfile.py
@@ -6,4 +6,4 @@ class QuickstartUser(HttpUser):
     def get_forecasts(self):
         lat = 56+randrange(20000)/10000 # 56 to 58, 4 decimal places
         lon = 20+randrange(80000)/10000 # 20 to 28, 4 decimal places
-        self.client.get(f"/api/v1/forecast/cities?lat={lat}&lon={lon}")
+        self.client.get(f"/api/v1/forecast/cities?lat={lat}&lon={lon}&add_aurora=True&add_params=False")
