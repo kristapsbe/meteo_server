8a54df276426279c3f95c08c3c9b5ed923637520
Kristaps <kbebris+git@gmail.com>
2024-12-24 00:35:32 +0200

    setting examples

--- settings.example.py
+++ settings.example.py
@@ -3,4 +3,8 @@ db_file = "meteo.db"
 if warning_mode:
     db_file = "meteo_warning_test.db"
 
+data_folder = "data/"
+if warning_mode:
+    data_folder = "data_warnings/"
+
 editdist_extension = '/path/to/.sqlpkg/sqlite/spellfix/spellfix.dylib'
