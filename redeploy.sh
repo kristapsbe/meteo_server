22613a7ccf76cfce5483ed01f4e48a2b7de16a9b
Kristaps <kbebris+git@gmail.com>
2025-01-26 10:59:42 +0200

    removed redundant values from version endpoint and only build image if changes have been pulled

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,12 @@
 cd ~/meteo_server
 dnf update -y # unsure if this is a good idea - may cause a lot of second long downtimes
 git checkout main
-git pull
-git rev-parse HEAD > app/version.txt
-docker compose build
-docker compose up -d
+
+changed=0
+git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then
+    git pull
+    git rev-parse HEAD > app/version.txt
+    docker compose build
+    docker compose up -d
+fi
