9f7b6778a064d00aa8b99603b0a86a0fdea880a3
Kristaps <kbebris+git@gmail.com>
2025-02-06 15:42:50 +0200

    getting rid of webi

--- utils/redeploy.sh
+++ utils/redeploy.sh
@@ -6,7 +6,7 @@ git fetch && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
     git pull
     git rev-parse HEAD > app/version.txt
-    docker compose build
+    docker compose build --build-arg HOST_ARCHITECTURE="$(uname -p)"
     docker compose rm -svf haproxy # making sure that the HAProxy container restarts so that new certs are picked up
     docker compose up -d
 
