2ea7025b2419cb11224ba7e3af3ce6a311ff46e8
Kristaps <kbebris+git@gmail.com>
2025-05-06 21:47:37 +0300

    removed q flag

--- haproxy/renew_certs.sh
+++ haproxy/renew_certs.sh
@@ -1,2 +1,2 @@
-uv run certbot certonly -n -q --standalone --force-renew -d meteo.kristapsbe.lv
+uv run certbot certonly -n --standalone --force-renew -d meteo.kristapsbe.lv
 cat /etc/letsencrypt/live/meteo.kristapsbe.lv/fullchain.pem /etc/letsencrypt/live/meteo.kristapsbe.lv/privkey.pem > /certs/haproxy.pem
