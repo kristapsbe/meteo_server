e328db671097220243fc9745abc535a18419e47f
Kristaps <kbebris+git@gmail.com>
2025-07-12 13:51:38 +0300

    poking around lt forecasts

--- app/utils/explore_meteo_lt.py
+++ app/utils/explore_meteo_lt.py
@@ -1,11 +1,52 @@
 import json
 import requests
 
+from utils import simlpify_string
+
+
 places = [e for e in json.loads(requests.get("https://api.meteo.lt/v1/places").content) if e['countryCode'] != "LV"]
-print(places)
+
+f_places = [{
+    'id': p['code'],
+    'source': 'LT',
+    'name': p['name'],
+    'search_name': simlpify_string(p['name'].strip().lower()),
+    'lat': p['coordinates']['latitude'],
+    'lon': p['coordinates']['longitude'],
+    'type': 'location_LT',
+    'county': p['administrativeDivision'],
+    'country': p['countryCode'],
+} for p in places]
+# print(f_places)
+
 countries = [p['countryCode'] for p in places]
-print({c: countries.count(c) for c in set(countries)}) # it's not just LT
+
+# hourly_params = [
+#     1, # Laika apstākļu ikona
+#     2, # Temperatūra
+#     3, # Sajūtu temperatūra
+#     4, # Vēja ātrums
+#     5, # Vēja virziens
+#     6, # Brāzmas
+#     7, # Nokrišņi
+#     #8, # Atmosfēras spiediens
+#     #9, # Gaisa relatīvais mitrums
+#     10, # UV indeks
+#     11, # Pērkona varbūtība
+# ]
+
+# daily_params = [
+#     #12, # Diennakts vidējais vēja virziens
+#     13, # Diennakts vidējā vēja vērtība
+#     14, # Diennakts maksimālā vēja brāzma
+#     15, # Diennakts maksimālā temperatūra
+#     16, # Diennakts minimālā temperatūra
+#     17, # Diennakts nokrišņu summa
+#     18, # Diennakts nokrišņu varbūtība
+#     19, # Laika apstākļu ikona nakti
+#     20, # Laika apstākļu ikona diena
+# ]
 
 for p in places:
-    print(json.loads(requests.get(f"https://api.meteo.lt/v1/places/{p['code']}/forecasts/long-term").content))
+    print([e['forecastTimeUtc'] for e in json.loads(requests.get(f"https://api.meteo.lt/v1/places/{p['code']}/forecasts/long-term").content)['forecastTimestamps']])
     break
