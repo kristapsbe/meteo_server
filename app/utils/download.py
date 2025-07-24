28e8a8b25f5963be04dce8839958b79f048eaea1
Kristaps <kbebris+git@gmail.com>
2025-07-24 18:51:24 +0300

    moving some code around

--- app/utils/download.py
+++ app/utils/download.py
@@ -8,7 +8,8 @@ import datetime
 import requests
 
 from time import sleep
-from utils import simlpify_string, hourly_params, daily_params, lt_daily_params, lt_day_icons, lt_hourly_params
+from utils import simlpify_string, hourly_params, daily_params
+from download_utils import lt_daily_params, lt_day_icons, lt_hourly_params, update_aurora_forecast
 from settings import db_file, data_folder, data_uptimerobot_folder, last_updated, run_emergency, run_emergency_failed
 
 
@@ -376,53 +377,6 @@ def update_db(update_time):
         logging.error(f"DB update FAILED - {e}")
     finally:
         upd_con.close()
-
-
-def update_aurora_forecast(update_time): # TODO: cleanup
-    url = "https://services.swpc.noaa.gov/json/ovation_aurora_latest.json"
-    fpath = f"{data_folder}ovation_aurora_latest.json"
-    times_fpath = f"{data_folder}ovation_aurora_times.json"
-
-    r = requests.get(url, timeout=10)
-    if r.status_code == 200:
-        with open(fpath, "wb") as f:
-            f.write(r.content)
-        aurora_data = json.loads(r.content)
-        with open(times_fpath, "w") as f:
-            f.write(json.dumps({
-                "Observation Time": aurora_data["Observation Time"],
-                "Forecast Time": aurora_data["Forecast Time"],
-            }))
-
-        upd_con = sqlite3.connect(db_file)
-        upd_cur = upd_con.cursor()
-        try:
-            upd_cur.execute("""
-                CREATE TABLE IF NOT EXISTS aurora_prob (
-                    lon INTEGER,
-                    lat INTEGER,
-                    aurora INTEGER,
-                    update_time INTEGER,
-                    PRIMARY KEY (lon, lat)
-                )
-            """)
-            upd_cur.executemany(f"""
-                INSERT INTO aurora_prob (lon, lat, aurora, update_time)
-                VALUES (?, ?, ?, {update_time})
-                ON CONFLICT(lon, lat) DO UPDATE SET
-                    aurora=excluded.aurora,
-                    update_time={update_time}
-            """, aurora_data["coordinates"])
-            logging.info(f"TABLE 'aurora_prob' - {upd_cur.rowcount} rows upserted")
-            upd_con.commit()
-            upd_cur.execute(f"DELETE FROM aurora_prob WHERE update_time < {update_time}")
-            logging.info(f"TABLE 'aurora_prob' - {upd_cur.rowcount} old rows deleted")
-            upd_con.commit()
-            logging.info("DB update finished")
-        except BaseException as e:
-            logging.error(f"DB update FAILED - {e}")
-        finally:
-            upd_con.close()
 
 
 def pull_uptimerobot_data(update_time):
