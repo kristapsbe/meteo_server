f0c227401b0245a90e70a5f7e1845fac57cd447b
Kristaps <kbebris+git@gmail.com>
2025-07-25 18:27:04 +0300

    moving some functions around

--- app/start.sh
+++ app/start.sh
@@ -1,12 +1,10 @@
 if [ ! -f /data/meteo.db ]; then
     sh run_job.sh download
-elif [ "$DO_RUN_AURORA" = "true" ]; then \
-    sh run_job.sh download_aurora
 fi
 
 crond
 if [ "$DO_RUN_AURORA" = "true" ]; then \
-    sh run_job.sh download_aurora & uv run main.py
+    sh run_job.sh download_every_20_minutes & uv run main.py
 else
     uv run main.py
 fi
--- app/utils/download_every_20_minutes.py
+++ app/utils/download_every_20_minutes.py
@@ -12,7 +12,12 @@ logging.basicConfig(
     format='%(asctime)s [%(levelname)s] %(message)s'
 )
 
-if __name__ == "__main__":
+
+def do_20_m_download():
     logging.info("Aurora download job starting")
     update_time = datetime.datetime.now(pytz.timezone('Europe/Riga')).strftime("%Y%m%d%H%M")
     update_aurora_forecast(update_time)
+
+
+if __name__ == "__main__":
+    do_20_m_download()
