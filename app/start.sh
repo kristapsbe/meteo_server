fbfe1561fd047d2a598aeaa736b93cd8fa0a27e4
Kristaps <kbebris+git@gmail.com>
2025-07-25 18:42:38 +0300

    renaming scripts

--- app/start.sh
+++ app/start.sh
@@ -4,7 +4,7 @@ fi
 
 crond
 if [ "$DO_RUN_AURORA" = "true" ]; then \
-    sh run_job.sh download_every_20_minutes & uv run main.py
+    sh run_job.sh download_small & uv run main.py
 else
     uv run main.py
 fi
