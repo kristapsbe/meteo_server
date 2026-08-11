623bae0238a0a96de2c725154ade5deff5b4c6ba
Kristaps <kbebris+git@gmail.com>
2026-08-11 14:29:26 +0300

    minor cleanup

--- app/utils/download_small.py
+++ app/utils/download_small.py
@@ -374,9 +374,10 @@ def pull_uptimerobot_data(update_time):
         )
         if r.status_code == 200:
             monit_data = json.loads(r.content)
-            open(
+            with open(
                 f"{data_uptimerobot_folder}uptimerobot_metrics_response.json", "wb"
-            ).write(r.content)
+            ) as mef:
+                mef.write(r.content)
             metrics = {k: {} for k in meta.values()}
             oldest_monit = min([e["create_datetime"] for e in monit_data["monitors"]])
             metrics["downtime"] = {oldest_monit: oldest_monit}
@@ -416,7 +417,7 @@ def pull_uptimerobot_data(update_time):
                                 )
                             }
                             if len(matches) > 0:
-                                for k in matches.keys():
+                                for k in matches:
                                     del metrics[ek][k]
                                 metrics[ek][
                                     min(min(matches.keys()), ent["datetime"])
@@ -476,20 +477,22 @@ def do_20_m_download(datasets, update_time):
 
     if skipped_empty and not os.path.isfile(run_emergency):
         logging.error("Failure encountered - setting emergency flag")
-        open(run_emergency, "w").write("")
+        with open(run_emergency, "w") as ref:
+            ref.write("")
 
     update_db(update_time)
     pull_uptimerobot_data(update_time)
 
     if not skipped_empty:
-        open(last_updated, "w").write(
-            datetime.datetime.fromtimestamp(
-                os.path.getmtime(f"{data_folder}{target_ds[0]}.json")
+        with open(last_updated, "w") as luf:
+            luf.write(
+                datetime.datetime.fromtimestamp(
+                    os.path.getmtime(f"{data_folder}{target_ds[0]}.json")
+                )
+                .replace(tzinfo=pytz.timezone("UTC"))
+                .astimezone(pytz.timezone("Europe/Riga"))
+                .strftime("%Y%m%d%H%M")
             )
-            .replace(tzinfo=pytz.timezone("UTC"))
-            .astimezone(pytz.timezone("Europe/Riga"))
-            .strftime("%Y%m%d%H%M")
-        )
         if os.path.isfile(run_emergency):
             os.remove(run_emergency)
         if os.path.isfile(run_emergency_failed):
